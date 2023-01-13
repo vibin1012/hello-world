6efed45e4ba5f3616aded053705c2aacfabcac4e
vibin1012 <122555614+vibin1012@users.noreply.github.com>
2023-01-13 12:05:40 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Fabulous)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.LeftTriangle)
+})
+basic.showIcon(IconNames.Heart)
