34f3ec54ee02876219e7a22aca4fb1ac72ee8d5c
vibin1012 <122555614+vibin1012@users.noreply.github.com>
2023-01-19 12:28:55 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Fabulous)
+    basic.showLeds(`
+        # . # # #
+        # . # . #
+        # . # . #
+        # . # . #
+        # . # # #
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showArrow(ArrowNames.North)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Yes)
+    basic.showString("Vibhav")
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
 })
