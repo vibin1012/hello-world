94777dc351d746b6c078a137166b0db8c17566c2
vibin1012 <122555614+vibin1012@users.noreply.github.com>
2023-01-20 12:19:14 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let rps = 0
+let math = 0
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         # . # # #
@@ -8,13 +9,7 @@ input.onButtonPressed(Button.A, function () {
         # . # # #
         `)
 })
-input.onButtonPressed(Button.AB, function () {
-    basic.showArrow(ArrowNames.North)
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showString("Vibhav")
-})
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.ScreenDown, function () {
     rps = randint(1, 3)
     if (rps == 1) {
         basic.showString("R")
@@ -22,6 +17,28 @@ input.onGesture(Gesture.Shake, function () {
         basic.showString("p")
     } else {
         basic.showString("S")
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showArrow(ArrowNames.North)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Vibhav")
+})
+input.onGesture(Gesture.Shake, function () {
+    math = randint(1, 6)
+    if (math == 1) {
+        basic.showString("4")
+    } else if (math == 2) {
+        basic.showString("6")
+    } else if (math == 3) {
+        basic.showString("7")
+    } else if (math == 4) {
+        basic.showString("8")
+    } else if (math == 5) {
+        basic.showString("6")
+    } else {
+        basic.showString("9")
     }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
