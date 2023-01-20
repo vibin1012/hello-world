be91501d02a2e0ddd7ce003d39106766826a61e8
vibin1012 <122555614+vibin1012@users.noreply.github.com>
2023-01-20 11:59:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let rps = 0
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         # . # # #
@@ -14,7 +15,14 @@ input.onButtonPressed(Button.B, function () {
     basic.showString("Vibhav")
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    rps = randint(1, 3)
+    if (rps == 1) {
+        basic.showString("R")
+    } else if (rps == 2) {
+        basic.showString("p")
+    } else {
+        basic.showString("S")
+    }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
